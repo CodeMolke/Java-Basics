e78d63d15df49014fe7c14a0610f1268fbcde030
CodeMolke <shubham.molke.dev@gmail.com>
2026-06-16 09:29:40 +0530

    Add Solution of Problem

--- Basics/Q10_PostDecrementation.java
+++ Basics/Q10_PostDecrementation.java
@@ -1,8 +1,8 @@
 public class Q10_PostDecrementation {
   public static void main(String[] args) {
-    int a= 20;
-    System.err.println(a--);
+    int a = 20;
+    System.out.println(a--);
     System.out.println(a);
   }
-  
+
 }

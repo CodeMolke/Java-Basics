398212f9944dd8f841c54787437ca03c2b33a402
CodeMolke <shubham.molke.dev@gmail.com>
2026-06-16 08:13:34 +0530

    Learning DataTypes

---
+++ Basics/Q1_StudentInformation.java
@@ -0,0 +1,12 @@
+public class Q1_StudentInformation {
+  public static void main(String[] args) {
+    String name = "Shubham";
+    int age = 24;
+    double percentage = 88.80;
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Percentage:" + percentage);
+
+  }
+
+}

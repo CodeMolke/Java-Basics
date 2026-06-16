5f8d70b8f7fc4cb10a195172aeb7ff03cfc0dda8
CodeMolke <shubham.molke.dev@gmail.com>
2026-06-16 10:27:51 +0530

    Add Solution of Area Of Circle  Problem

---
+++ Basics/Q15_AreaOfCircle.java
@@ -0,0 +1,12 @@
+public class Q15_AreaOfCircle {
+  public static void main(String[] args) {
+    double radius = 7;
+    double pi = 3.14159;
+    double area = pi * radius * radius ;
+
+    System.out.println("Radius of Circle: "+ radius);
+    System.out.println("Area of Circle :" + area);
+
+  }
+  
+}

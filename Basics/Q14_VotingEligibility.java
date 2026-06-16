81d2d8b0f147f275e0629f96bf34f6eee9d8fc60
CodeMolke <shubham.molke.dev@gmail.com>
2026-06-16 10:22:28 +0530

    Add Solution of VotingEligibility  Problem

---
+++ Basics/Q14_VotingEligibility.java
@@ -0,0 +1,11 @@
+public class Q14_VotingEligibility {
+  public static void main(String[] args) {
+    int age = 17;
+    if (age > 18) {
+      System.out.println("Candidate Is Eligible For Voting !");
+    } else {
+      System.out.println("Candidate is Not Eligible For Voting !");
+    }
+  }
+
+}

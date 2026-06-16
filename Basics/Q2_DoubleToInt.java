8ff923b8d5e6e58099e49df0eb95c7b52bc98a1c
CodeMolke <shubham.molke.dev@gmail.com>
2026-06-16 08:26:11 +0530

    Add type casting example

---
+++ Basics/Q2_DoubleToInt.java
@@ -0,0 +1,12 @@
+public class Q2_DoubleToInt {
+  public static void main(String[] args) {
+    
+    double marks = 89.75;
+
+    int convertedMarks =(int) marks;
+    System.out.println("Original Marks : " + marks);
+    System.out.println("Converted Marks : "+ convertedMarks);
+
+  }
+  
+}

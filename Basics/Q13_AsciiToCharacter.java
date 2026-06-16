a5c98b15df1fec98554b29a711046805d9c36061
CodeMolke <shubham.molke.dev@gmail.com>
2026-06-16 10:18:18 +0530

    Add Solution of AsciiToCharacter  Problem

---
+++ Basics/Q13_AsciiToCharacter.java
@@ -0,0 +1,8 @@
+public class Q13_AsciiToCharacter {
+  public static void main(String[] args) {
+    int ascii = 65;
+     char converted = (char)ascii;
+     System.out.println("Character Conversion Of ASCII "+ ascii+" is "+ converted);
+  }
+  
+}

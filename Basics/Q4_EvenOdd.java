82a3f4205ef636d34e1420680415a1794fd0dfc3
CodeMolke <shubham.molke.dev@gmail.com>
2026-06-16 08:44:05 +0530

    Add Solution For Odd-Even Number Problem

---
+++ Basics/Q4_EvenOdd.java
@@ -0,0 +1,12 @@
+public class Q4_EvenOdd {
+  public static void main(String[] args) {
+    int number = 17;
+    if(number % 2  ==0){
+      System.out.println("EVEN");
+      }
+      else{
+        System.out.println("ODD");
+    }
+  }
+  
+}

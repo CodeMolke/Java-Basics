7bd8572b6f815c439c7ac922b20bd70c5d26365f
CodeMolke <shubham.molke.dev@gmail.com>
2026-06-19 17:57:24 +0530

    Solution of Sum Of Namtural Numbers Problem

---
+++ Basics/Q21_SumOfNaturalNumbers.java
@@ -0,0 +1,11 @@
+public class Q21_SumOfNaturalNumbers {
+  public static void main(String[] args) {
+    int sum = 0;
+    for (int i = 1; i <= 100; i++) {
+      sum = sum + i;
+    }
+    System.out.println("Sum Of First Natural Numbers is : " + sum);
+
+  }
+
+}

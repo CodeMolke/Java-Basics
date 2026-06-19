ef953cc5d81f92bf4b2021789e75b1448e66ea79
CodeMolke <shubham.molke.dev@gmail.com>
2026-06-19 12:57:20 +0530

    Solution Of Positive,Negative, Or Zero Problem

---
+++ Basics/Q16_PositiveNegativeZero.java
@@ -0,0 +1,17 @@
+public class Q16_PositiveNegativeZero {
+
+  public static void main(String[] args) {
+    int number = -10;
+    if (number>0)
+    {
+      System.out.println("Number is Postive");
+    }
+    else if (number<0) {
+      System.out.println("Number is Negative");
+      
+    }
+    else{
+      System.out.println("Number is Zero");
+    }
+  }
+}

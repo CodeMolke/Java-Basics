3ecc5e4a4944fb40555e02010d8da654e53d6f6f
CodeMolke <shubham.molke.dev@gmail.com>
2026-06-19 18:25:02 +0530

    Solution of Reverse Counting Problem

---
+++ Basics/Q23_ReverseNumbers.java
@@ -0,0 +1,13 @@
+public class Q23_ReverseNumbers {
+  public static void main(String[] args) {
+    int i = 20 ;
+    while (i >= 1){
+      System.out.println(i);
+      i--;
+
+    }
+  }
+
+  }
+  
+

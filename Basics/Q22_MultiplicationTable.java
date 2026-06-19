e4468ef806ef1490753d277b012a90c5bb13a4ad
CodeMolke <shubham.molke.dev@gmail.com>
2026-06-19 18:10:51 +0530

    Solution of Multiplication Problem

---
+++ Basics/Q22_MultiplicationTable.java
@@ -0,0 +1,11 @@
+public class Q22_MultiplicationTable {
+  public static void main(String[] args) {
+    int result = 0;
+    for (int i = 1; i <= 10; i++) {
+      result = 7 * i;
+      System.out.println("7 X " + i + " = " + result);
+    }
+
+  }
+
+}

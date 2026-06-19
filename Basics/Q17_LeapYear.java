3a8d911287e6c033930c3855dd5c4f7b5cceea17
CodeMolke <shubham.molke.dev@gmail.com>
2026-06-19 13:11:37 +0530

    Solution of Leap Year Problem

---
+++ Basics/Q17_LeapYear.java
@@ -0,0 +1,16 @@
+public class Q17_LeapYear {
+  public static void main(String[] args) {
+
+    int year = 2024;
+
+    if (year % 400 == 0) {
+      System.out.println("Leap Year");
+    } else if (year % 4 == 0 && year % 100 != 0) {
+      System.out.println("Leap Year");
+
+    } else {
+      System.out.println("Not An Leap Year");
+    }
+
+  }
+}
